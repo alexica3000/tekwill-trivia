639ab46ff173e46c137deb13ff9e452b49984944
alex s <sanatateacom@gmail.com>
2021-01-04 21:39:45 +0200

    select start or exit the game

--- src/com/tekwill/learning/trivia/Game.java
+++ src/com/tekwill/learning/trivia/Game.java
@@ -1,4 +1,26 @@
 package com.tekwill.learning.trivia;
 
+import java.util.Scanner;
+
 public class Game {
+    static boolean gameMenu = true;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter START to start the game or EXIT for exit.");
+
+        do {
+            String enter = scanner.nextLine();
+
+            if(enter.equals("START")) {
+                System.out.println("Start game...");
+            } else if (enter.equals("EXIT")) {
+                System.out.println("End game...");
+                gameMenu = false;
+            } else {
+                System.out.println("Please enter START or EXIT.");
+            }
+        } while (gameMenu);
+    }
 }
